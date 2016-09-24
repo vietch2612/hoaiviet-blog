2ec88e0af92181d138d40196c63e3e806d32a70c
Cao Hoai Viet <hoaiviet@siliconstraits.com>
2016-09-24 21:07:41 +0700

    User can create comment

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,9 @@
 class Article < ApplicationRecord
+  # attr_accessible :title, :body
   is_impressionable
+
+  has_many :comments
+
   def self.search(search)
     where("title LIKE ?", "%#{search}%")
     # where("body LIKE ?", "%#{search}%")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924034119) do
+ActiveRecord::Schema.define(version: 20160924071925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,15 @@ ActiveRecord::Schema.define(version: 20160924034119) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "article_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["article_id"], name: "index_comments_on_article_id", using: :btree
   end
 
   create_table "impressions", force: :cascade do |t|
@@ -48,4 +57,5 @@ ActiveRecord::Schema.define(version: 20160924034119) do
     t.index ["user_id"], name: "index_impressions_on_user_id", using: :btree
   end
 
+  add_foreign_key "comments", "articles"
 end
